51f26be7699bcd7fd6b8fae8630ed321b68a6593
David Holeman <david.holeman@opexnetworks.com>
2020-06-08 13:47:11 -0700

    Initial Commit

---
+++ Authentication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Authentication
+//
+//  Created by David Holeman on 6/8/20.
+//  Copyright © 2020 Opex Networks, LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
